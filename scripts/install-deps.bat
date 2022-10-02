bdad4b1cc34241e2f9b724cbb98e38f7e5966f31
mgn901 <mgn901+dev@gmail.com>
2022-10-02 21:33:48 +0900

    Install web fonts from npm

--- scripts/install-deps.bat
+++ scripts/install-deps.bat
@@ -1,2 +1,2 @@
-call npm i --save react react-dom react-transition-group
+call npm i --save @material-design-icons/font inter-ui react react-dom react-transition-group
 call npm i --save-dev @types/node @types/react @types/react-dom @types/react-transition-group autoprefixer esbuild esbuild-copy-static-files esbuild-register esbuild-style-plugin postcss tailwindcss typescript
